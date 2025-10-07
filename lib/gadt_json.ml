fc8a0be613ff850dd9a53411230f6acda6e1ec7a
Bruno Flores <bruno@brunoflores.com.br>
2025-10-08 04:57:50 +1000

    Add typed JSON.

--- lib/gadt_json.ml
+++ lib/gadt_json.ml
@@ -4,3 +4,14 @@ type ujson =
   | UBool : bool -> ujson
   | UNull : ujson
   | UArr : ujson list -> ujson
+
+type _ tjson =
+  | Str : string -> string tjson
+  | Num : float -> float tjson
+  | Bool : bool -> bool tjson
+  | Null : unit tjson
+  | Arr : 'a tarr -> 'a tjson
+
+and _ tarr =
+  | Nil : unit tarr
+  | ( :: ) : 'a tjson * 'b tarr -> ('a * 'b) tarr
