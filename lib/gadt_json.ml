89255a224ee3853716dba078b43115b8e86b2207
Bruno Flores <bruno@brunoflores.com.br>
2025-10-08 05:31:20 +1000

    Typed JSON from untyped JSON using existentials.

--- lib/gadt_json.ml
+++ lib/gadt_json.ml
@@ -15,3 +15,23 @@ type _ tjson =
 and _ tarr =
   | Nil : unit tarr
   | ( :: ) : 'a tjson * 'b tarr -> ('a * 'b) tarr
+
+(* Building typed JSON from untyped JSON using existentials *)
+type etjson = ETJson : 'a tjson -> etjson
+type etarr = ETArr : 'a tarr -> etarr
+
+let rec tjson_of_ujson : ujson -> etjson = function
+  | UStr s -> ETJson (Str s)
+  | UNum n -> ETJson (Num n)
+  | UBool b -> ETJson (Bool b)
+  | UNull -> ETJson Null
+  | UArr arr ->
+      let (ETArr arr') = tarr_of_uarr arr in
+      ETJson (Arr arr')
+
+and tarr_of_uarr : ujson list -> etarr = function
+  | [] -> ETArr Nil
+  | j :: js ->
+      let (ETJson j') = tjson_of_ujson j in
+      let (ETArr js') = tarr_of_uarr js in
+      ETArr (j' :: js')
