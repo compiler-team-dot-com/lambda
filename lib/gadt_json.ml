e9ce6119047f8649d24d688e8e0868377d434fa1
Bruno Flores <bruno@brunoflores.com.br>
2025-10-12 10:15:02 +1000

    Separate types and data.

--- lib/gadt_json.ml
+++ lib/gadt_json.ml
@@ -35,3 +35,83 @@ and tarr_of_uarr : ujson list -> etarr = function
       let (ETJson j') = tjson_of_ujson j in
       let (ETArr js') = tarr_of_uarr js in
       ETArr (j' :: js')
+
+type _ tyjson =
+  | TyStr : string tyjson
+  | TyNum : float tyjson
+  | TyBool : bool tyjson
+  | TyNull : 'a tyjson -> 'a option tyjson
+  | TyArr : 'a tyarr -> 'a tyjson
+
+and _ tyarr =
+  | TyNil : unit tyarr
+  | ( :: ) : 'a tyjson * 'b tyarr -> ('a * 'b) tyarr
+
+let _ =
+  ( TyArr
+      (TyStr :: TyBool :: TyNum
+      :: TyArr (TyArr (TyStr :: TyNil) :: TyNull TyBool :: TyNil)
+      :: TyNil),
+    ("one", (true, (3.4, ((("four", ()), (None, ())), ())))) )
+
+(* The negate function, entangled *)
+let rec negate : type a. a tjson -> a tjson = function
+  | Bool b -> Bool (not b)
+  | Arr arr -> Arr (negate_arr arr)
+  | v -> v
+
+and negate_arr : type a. a tarr -> a tarr = function
+  | Nil -> Nil
+  | j :: js -> negate j :: negate_arr js
+
+(* The negate function, disentangled *)
+let rec negateD : type a. a tyjson -> a -> a =
+ fun t v ->
+  match (t, v) with
+  | TyBool, true -> false
+  | TyBool, false -> true
+  | TyArr a, arr -> negate_arrD a arr
+  | TyNull j, Some v -> Some (negateD j v)
+  | TyNull _, None -> None
+  | _, v -> v
+
+and negate_arrD : type a. a tyarr -> a -> a =
+ fun t v ->
+  match (t, v) with
+  | TyNil, () -> ()
+  | j :: js, (a, b) -> (negateD j a, negate_arrD js b)
+
+let _ =
+  negateD
+    (TyArr
+       (TyStr :: TyBool :: TyNum
+       :: TyArr (TyArr (TyStr :: TyNil) :: TyNull TyBool :: TyNil)
+       :: TyNil))
+    ("one", (true, (3.4, ((("four", ()), (None, ())), ()))))
+
+(* The negate function, disentangled and "staged" *)
+let id x = x
+let map_option f = function None -> None | Some x -> Some (f x)
+
+let rec negateDS : type a. a tyjson -> a -> a = function
+  | TyBool -> not
+  | TyArr a -> negate_arrDS a
+  | TyNull j -> map_option (negateDS j)
+  | _ -> id
+
+and negate_arrDS : type a. a tyarr -> a -> a = function
+  | TyNil -> id
+  | j :: js ->
+      let n = negateDS j in
+      let ns = negate_arrDS js in
+      fun (a, b) -> (n a, ns b)
+
+let _ =
+  let staged =
+    negateDS
+      (TyArr
+         (TyStr :: TyBool :: TyNum
+         :: TyArr (TyArr (TyStr :: TyNil) :: TyNull TyBool :: TyNil)
+         :: TyNil))
+  in
+  staged ("one", (true, (3.4, ((("four", ()), (None, ())), ()))))
