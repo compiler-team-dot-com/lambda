2d7f4ff00fe5b38052c33e03a12ecd84616da5bd
Bruno Flores <bruno@brunoflores.com.br>
2025-10-24 06:31:30 +1000

    Continue.

--- lib/defunctionalization.ml
+++ lib/defunctionalization.ml
@@ -102,3 +102,114 @@ let list_seq : ('a, list_name) seq =
       (fun (List_name l) ->
         match l with [] -> None | h :: t -> Some (h, List_name t));
   }
+
+(* Algebras *)
+
+(* DSL of addition *)
+module type add_i = sig
+  type repr
+
+  val int : int -> repr
+  val add : repr -> repr -> repr
+end
+
+(* Term as a functor *)
+module AddEx1 (I : add_i) = struct
+  open I
+
+  let res = add (add (int 1) (int 2)) (int 3)
+end
+
+(* A function that uses the add interfacce: parameterized by the
+implementation of the interface: add a term to itself n times. *)
+module NTimes (I : add_i) = struct
+  include I
+
+  let rec ntimes : int -> repr -> repr =
+   fun n x ->
+    if n <= 0 then int 0 else if n = 1 then x else add x (ntimes (n - 1) x)
+end
+
+(* Usage example, using the earlier AddEx1 *)
+module AddEx2 (I : add_i) = struct
+  open NTimes (I)
+  module M = AddEx1 (I)
+
+  let res = ntimes 3 M.res
+end
+
+(* All functors. We would like terms.
+Using first-class modules, turn add_i (a sig) to an ordinary type. *)
+type 'r add_t = (module add_i with type repr = 'r)
+
+module type sym = sig
+  type 'a repr
+
+  val int : int -> int repr
+  val add : int repr -> int repr -> int repr
+  val iszero : int repr -> bool repr
+  val if_ : bool repr -> 'a repr -> 'a repr -> 'a repr
+end
+
+(* a sample term of the DSL *)
+module SymEx1 (I : sym) = struct
+  open I
+
+  let t1 = add (add (int 1) (int 2)) (int 3) (* intermediate binding *)
+  let res = if_ (iszero t1) (int 0) (add t1 (int 1))
+end
+
+module type symF = sig
+  type a
+
+  module Term (I : sym) : sig
+    val res : a I.repr
+  end
+end
+
+type 'a sym_term = (module symF with type a = 'a)
+
+let sym_ex1 : _ sym_term =
+  (module struct
+    type a = int
+
+    module Term = SymEx1
+  end)
+
+(* evaluation: a bit cumbersome *)
+let _ =
+  let module N = (val sym_ex1) in
+  let module M = N.Term (R) in
+  M.res
+
+module SymSelf : sym with type 'a repr = 'a sym_term = struct
+  type 'a repr = 'a sym_term
+
+  let int : int -> int repr =
+   fun n ->
+    let module M (I : sym) = struct
+      let res = I.int n
+    end in
+    (module struct
+      type a = int
+
+      module Term = M
+    end)
+
+  let add : int repr -> int repr -> int repr =
+   fun (module E1) (module E2) ->
+    let module M (I : sym) = struct
+      module E1T = E1.Term (I)
+      module E2T = E2.Term (I)
+
+      let res = I.add E1T.res E2T.res
+    end in
+    (module struct
+      type a = int
+
+      module Term = M
+    end)
+
+  let iszero : int repr -> bool repr = failwith ""
+  let if_ : bool repr -> 'a repr -> 'a repr -> 'a repr = failwith ""
+end
