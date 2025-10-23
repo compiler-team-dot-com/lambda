69473238d91117c486275e1e7245ecc98e6ef03c
Bruno Flores <bruno@brunoflores.com.br>
2025-10-24 05:06:32 +1000

    Continue from https://okmij.org/ftp/ML/higher-kind-poly.html

--- lib/defunctionalization.ml
+++ lib/defunctionalization.ml
@@ -28,3 +28,35 @@ let sum l = fold (Fn_plus, 0, l)
 let add (n, l) = fold (Fn_plus_cons n, [], l)
 let[@warning "-8"] 3 = sum [ 1; 1; 1 ]
 let[@warning "-8"] [ 2; 2; 2 ] = add (1, [ 1; 1; 1 ])
+
+(* Higher kind poly *)
+(* https://okmij.org/ftp/ML/higher-kind-poly.html *)
+
+let rec sumi : int list -> int = function [] -> 0 | h :: t -> h + sumi t
+
+let rec foldi (f : int -> int -> int) (z : int) : int list -> int = function
+  | [] -> z
+  | h :: t -> f h (foldi f z t)
+
+let rec fold (f : 'a -> 'a -> 'a) (z : 'a) : 'a list -> 'a = function
+  | [] -> z
+  | h :: t -> f h (foldi f z t)
+
+type 'a monoid = {
+  op : 'a -> 'a -> 'a;
+  unit : 'a;
+}
+
+(* bounded polymorphism *)
+let rec foldm (m : 'a monoid) : 'a list -> 'a = function
+  | [] -> m.unit
+  | h :: t -> m.op h (foldm m t)
+
+(* This won't work:
+
+type ('a,'F) seq = {decon: 'a 'F -> ('a * 'a 'F) option}
+
+let rec folds (m: 'a monoid) (s: ('a,'F) seq) : 'a 'F -> 'a = fun c ->
+  match s.decon c with None -> m.unit | Some (h,t) -> m.op h (folds m s t)
+
+*)
