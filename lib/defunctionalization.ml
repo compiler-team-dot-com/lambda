fe9b756bbe97b26a50945d9d24a9ba7aa230d9ea
Bruno Flores <bruno@brunoflores.com.br>
2025-10-24 05:53:39 +1000

    Continue

--- lib/defunctionalization.ml
+++ lib/defunctionalization.ml
@@ -52,6 +52,12 @@ let rec foldm (m : 'a monoid) : 'a list -> 'a = function
   | [] -> m.unit
   | h :: t -> m.op h (foldm m t)
 
+(* Examples *)
+let monoid_plus : int monoid = { op = ( + ); unit = 0 }
+let[@warning "-8"] 6 = foldm monoid_plus [ 1; 2; 3 ]
+let monoid_maxchar : char monoid = { op = max; unit = Char.chr 0 }
+let[@warning "-8"] 'c' = foldm monoid_maxchar [ 'b'; 'c'; 'a' ]
+
 (* This won't work:
 
 type ('a,'F) seq = {decon: 'a 'F -> ('a * 'a 'F) option}
@@ -60,3 +66,39 @@ let rec folds (m: 'a monoid) (s: ('a,'F) seq) : 'a 'F -> 'a = fun c ->
   match s.decon c with None -> m.unit | Some (h,t) -> m.op h (folds m s t)
 
 *)
+
+(* Using the module language: *)
+module type seq_i = sig
+  type 'a t
+
+  val decon : 'a t -> ('a * 'a t) option
+end
+
+module FoldS (S : seq_i) = struct
+  let rec fold (m : 'a monoid) : 'a S.t -> 'a =
+   fun c ->
+    match S.decon c with None -> m.unit | Some (h, t) -> m.op h (fold m t)
+end
+
+(* Second approach *)
+type ('a, 'b) app = ..
+type list_name
+type ('a, 'b) app += List_name : 'a list -> ('a, list_name) app
+
+let inj x = List_name x
+let[@warning "-8"] prj (List_name x) = x
+
+type ('a, 'n) seq = { decon : ('a, 'n) app -> ('a * ('a, 'n) app) option }
+
+let rec folds (m : 'a monoid) (s : ('a, 'n) seq) : ('a, 'n) app -> 'a =
+ fun c ->
+  match s.decon c with None -> m.unit | Some (h, t) -> m.op h (folds m s t)
+
+let sums s c = folds monoid_plus s c
+
+let list_seq : ('a, list_name) seq =
+  {
+    decon =
+      (fun (List_name l) ->
+        match l with [] -> None | h :: t -> Some (h, List_name t));
+  }
